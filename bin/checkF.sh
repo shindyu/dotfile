042967b66a05ca67f044b98bc740b7c69b6e09d0
shindyu <shindyu@shindyu-no-MacBook-Pro.local>
2016-11-10 22:12:57 +0900

    Added echo message to checkF.sh

--- bin/checkF.sh
+++ bin/checkF.sh
@@ -1,1 +1,9 @@
-find $* -type f -name "*.swift" -print | xargs grep -rnw -e "fdescribe\|fcontext\|fit" --color=auto
+# search strings
+pattern="fdescribe\|fcontext\|fit"
+
+# display filename and line number
+if find $* -type f -name "*.swift" -print | xargs grep -rnw -e $pattern --color=auto
+then
+    echo "***** All Tests are not done. *****"
+    echo "***** Don't commit or push.   *****"
+fi
